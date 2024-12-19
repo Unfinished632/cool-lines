108dc8c8ef96553ac401fe00527739bfb66ce6d4
Unfinished632 <username1653e632@gmail.com>
2024-12-20 00:02:38 +0300

    Added ImGUI

---
+++ src/engine.h
@@ -0,0 +1,9 @@
+#include <raylib.h>
+#include <imgui.h>
+#include <rlImGui.h>
+
+class Engine{
+    public:
+    Engine();
+    ~Engine();
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,39 @@
 #include <raylib.h>
+#include <imgui.h>
+#include <rlImGui.h>
+
+#include "buildConfig.h"
 
 int main(){
+    #ifdef RELEASE_BUILD
+        SetTraceLogLevel(LOG_NONE);
+    #endif
+
+
     InitWindow(800, 450, "raylib [core] example - basic window");
+
+    IMGUI_CHECKVERSION();
+
+    rlImGuiSetup(true);
 
     while(!WindowShouldClose()){
         BeginDrawing();
+        rlImGuiBegin();
 
         ClearBackground(BLACK);
-        DrawText("Hello World!", 10, 10, 50, RAYWHITE);
+        DrawText(TextFormat("%d", GetFPS()), 10, 10, 50, RAYWHITE);
 
+        ImGui::Begin("Settings");
+
+        ImGui::Button("Hello World!");
+
+        ImGui::End();
+
+        rlImGuiEnd();
         EndDrawing();
     }
+
+    rlImGuiShutdown();
 
     CloseWindow();
 
